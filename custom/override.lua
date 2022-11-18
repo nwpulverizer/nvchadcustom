ae3c0fe80a4c6ce92deb77b5705e4040c6211be5
Nathan <nathan.w.pulver@gmail.com>
2022-11-18 12:42:56 -0800

    initial commit

---
+++ custom/override.lua
@@ -0,0 +1,34 @@
+local M = {}
+
+M.nvimtree = {
+  git = {
+    enable = true,
+  },
+  renderer = {
+    highlight_git = true,
+    icons = {
+      show = {
+        git = true,
+      },
+    },
+  },
+  view = {
+    side = "right",
+  },
+}
+
+M.blankline = {
+  filetype_exclude = {
+    "help",
+    "terminal",
+    "alpha",
+    "packer",
+    "lspinfo",
+    "TelescopePrompt",
+    "TelescopeResults",
+    "nvchad_cheatsheet",
+    "lsp-installer",
+    "",
+  }, }
+
+return M
